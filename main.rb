a7d1e69d9f39ae1c2430304a7758bf3c285da375
Balolebwami <tchasingajacques@gmail.com>
2023-09-05 10:02:23 +0200

    new organization

--- main.rb
+++ main.rb
@@ -8,7 +8,7 @@ person = Person.new(1, 'Alice', 20)
 student = Student.new(2, 'Class A')
 teacher = Teacher.new(3, 'Math')
 
-# Use the methods and properties of the objects
+# Use the methods and properties 
 puts "Person can use services: #{person.can_use_services?}"
 puts "Student can use services: #{student.can_use_services?}"
 puts "Teacher can use services: #{teacher.can_use_services?}"
